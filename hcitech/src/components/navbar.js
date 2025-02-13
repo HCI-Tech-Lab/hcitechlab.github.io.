2aed32370b10001269605b2d9093adbd245d4a13
Murad Eynizada <eynizadamurad@gmail.com>
2025-02-13 13:20:17 +0900

    fixes to the navbar

--- hcitech/src/components/navbar.js
+++ hcitech/src/components/navbar.js
@@ -8,26 +8,26 @@ const NavBar = () => {
     return (
         <div className = "container">
             <nav className = "navbar navbar-expand-lg navbar-light px-3 fixed-top bg-white shadow-sm">
-                <div style={{ width: "110px"}}>
-
+                <div className = "container">
+                    <Link href = '/' className = 'navbar-brand'>
+                        <Image alt = "logo" src = "/main_logo.png" width = {300} height = {50} className = "d-none d-sm-block"/>
+                    </Link>
+                    <div>
+                        <ul className = "navbar-nav me-auto">
+                        {[
+                            { path: '/publications', label: 'Publications' },
+                            { path: '/team', label: 'Team' },
+                            { path: '/courses', label: 'Courses' },
+                            { path: '/gallery', label: 'Gallery' },
+                            { path: '/contact', label: 'Contact' },
+                            { path: '/news', label: 'News' },
+                        ].map((item) => (
+                        <li className="nav-item" key = {item.path}> <Link href={item.path} className={`nav-link ${path == item.path ? "active" : ""}`}> {item.label} </Link> </li>
+                        ))}
+                        </ul>
+                    </div>
                 </div>
-                <Link href = '/' className = 'navbar-brand'>
-                    <Image alt = "logo" src = "/main_logo.png" width = {300} height = {50} className = "d-none d-sm-block"/>
-                </Link>
-                <div>
-                    <ul className = "navbar-nav me-auto">
-                    {[
-                        { path: '/publications', label: 'Publications' },
-                        { path: '/team', label: 'Team' },
-                        { path: '/courses', label: 'Courses' },
-                        { path: '/gallery', label: 'Gallery' },
-                        { path: '/contact', label: 'Contact' },
-                        { path: '/news', label: 'News' },
-                    ].map((item) => (
-                    <li className="nav-item" key = {item.path}> <Link href={item.path} className={`nav-link ${path == item.path ? "active" : ""}`}> {item.label} </Link> </li>
-                    ))}
-                    </ul>
-                </div>
+                
             </nav>
         </div>
     )

6e52ecfa8f19d64dc835be4d5ce82601d4bbff2b
muradeynizada <eynizadamurad@gmail.com>
2025-03-27 14:37:34 +0900

    fixes to config

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/updatetest' : '',
-  basePath: isProd ? '/updatetest' : '',
   output: 'export'
 };
 
